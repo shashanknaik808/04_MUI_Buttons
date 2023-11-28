fa9d60289b9a008faa75ef7e4af16164911ce2bb
Shashank Naik <shashanknaik808@gmail.com>
2023-11-28 15:05:58 +0530

    Add Test component rendering 'Hello World' with Typography

---
+++ mui-tutorial/src/Test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+function Test() {
+    return (
+        <div>
+            <Typography>
+                Hello World
+            </Typography>
+        </div>
+    )
+}
+
+export default Test;
