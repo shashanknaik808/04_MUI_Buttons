bca4ae81a755b2b4ea1f7cf07e91b452ed813840
Shashank Naik <shashanknaik808@gmail.com>
2023-11-28 15:34:17 +0530

    Implement Material UI Button Component

--- mui-tutorial/src/Test.jsx
+++ mui-tutorial/src/Test.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Button } from '@mui/material';
 
 function Test() {
     return (
         <div>
-            <Typography variant='h5'>
-                Hello World
-            </Typography>
+            <Button onClick={() => alert("Button Clicked")}
+                color='success' sx={{ margin: 3 }} size='large'
+                variant='contained'>First
+            </Button>
+            <Button onClick={() => alert("Button Clicked")}
+                color='error' sx={{ margin: 3 }} size='medium'
+                variant='outlined'>Second
+            </Button>
+            <Button onClick={() => alert("Button Clicked")}
+                color='info' sx={{ margin: 3 }} size='small'
+                variant='text'>Third
+            </Button>
         </div>
     )
 }
